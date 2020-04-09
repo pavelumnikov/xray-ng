9bab541d1a15277216eca682730c5b45b1acd939
Pavel <codepoet.vault@gmail.com>
2020-04-09 13:58:09 +0300

    GLFW module now exports Vulkan to user modules

--- scripts/cmake-vs2019-64bit-static.bat
+++ scripts/cmake-vs2019-64bit-static.bat
@@ -4,10 +4,11 @@ if errorlevel 1 echo Unable to enable extensions
 set CMAKE_PATH=%ROOTDIR%\extras\cmake
 set BUILD_ROOT=%ROOTDIR%\projects\%~n0
 if "%PHYSX_DIR%"=="" ECHO PHYSX_DIR is NOT defined
+if "%VK_SDK_PATH%"=="" ECHO VK_SDK_PATH is NOT defined
 del /s /q %BUILD_ROOT%
 mkdir %BUILD_ROOT%
 pushd %BUILD_ROOT%
-%CMAKE_PATH%\bin\cmake.exe -G "Visual Studio 16 2019" -DWITH_TESTS=1 -DWITH_EDITOR=1 -DPHYSX_DIR="%PHYSX_DIR%\install\vc15win64" --check-system-vars %ROOTDIR%
+%CMAKE_PATH%\bin\cmake.exe -G "Visual Studio 16 2019" -DWITH_TESTS=1 -DWITH_EDITOR=1 -DPHYSX_DIR="%PHYSX_DIR%\install\vc15win64" -DVK_SDK_PATH="%VK_SDK_PATH%" --check-system-vars %ROOTDIR%
 popd
 pause
 endlocal
