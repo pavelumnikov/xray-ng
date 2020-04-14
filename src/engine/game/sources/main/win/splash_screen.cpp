8204672afa276cb15c87f9ce9b93a27a93f335eb
Pavel <codepoet.vault@gmail.com>
2020-04-14 12:56:38 +0300

    main: code and code-style fixes (for all previous changes)

--- src/engine/game/sources/main/win/splash_screen.cpp
+++ src/engine/game/sources/main/win/splash_screen.cpp
@@ -1,37 +1,12 @@
-/*
-
-  Copyright (c) 2019, Pavel Umnikov
-  All rights reserved.
-
-  Redistribution and use in source and binary forms, with or without
-  modification, are permitted provided that the following conditions are met:
-
-  * Redistributions of source code must retain the above copyright notice,
-    this list of conditions and the following disclaimer.
-  * Redistributions in binary form must reproduce the above copyright
-    notice, this list of conditions and the following disclaimer in the
-    documentation and/or other materials provided with the distribution.
-
-  THIS SOFTWARE IS PROVIDED BY THE AUTHOR AND CONTRIBUTORS ``AS IS'' AND ANY
-  EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
-  WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
-  DISCLAIMED. IN NO EVENT SHALL THE AUTHOR OR CONTRIBUTORS BE LIABLE FOR ANY
-  DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
-  (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
-  SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
-  CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
-  LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY
-  OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH
-  DAMAGE.
-
-*/
+// This file is a part of xray-ng engine
+//
 
 #include "splash_screen.h"
 #include "corlib/sys/thread.h"
 #include "resource.h"
 #include <Windows.h>
 
-//------------------------------------------------------------------------------
+//-----------------------------------------------------------------------------------------------------------
 namespace xr::game::main::details
 {
 
@@ -47,7 +22,7 @@ struct splash_screen_context final
     bool handle_valid { false };
 };
 
-//------------------------------------------------------------------------------
+//-----------------------------------------------------------------------------------------------------------
 INT_PTR CALLBACK window_procedure(HWND hwnd, UINT msg, WPARAM wp, LPARAM lp)
 {
     XR_UNREFERENCED_PARAMETER(wp, lp, hwnd);
@@ -61,8 +36,8 @@ INT_PTR CALLBACK window_procedure(HWND hwnd, UINT msg, WPARAM wp, LPARAM lp)
     return 0;
 };
 
-//------------------------------------------------------------------------------
-void on_splash_screen(void* const arg) noexcept
+//-----------------------------------------------------------------------------------------------------------
+uint32_t on_splash_screen(void* const arg) noexcept
 {
     XR_DEBUG_ASSERTION(arg != nullptr);
     auto const splash_screen = static_cast<splash_screen_context*>(arg);
@@ -74,7 +49,7 @@ void on_splash_screen(void* const arg) noexcept
     if(!bitmap_handle)
     {
         //LOG_ERROR("cannot create bitmap for splash screen from file \"%s\" ", file_name.c_str());
-        return;
+        return 1;
     }
 
     BITMAP bm; // get bitmap data
@@ -135,23 +110,24 @@ void on_splash_screen(void* const arg) noexcept
     // but it is more safe to do this here always
 
     DeleteObject(bitmap_handle);
+    return 0;
 }
 
 } // namespace xr::game::main::details
 
-//------------------------------------------------------------------------------
+//-----------------------------------------------------------------------------------------------------------
 namespace xr::game::main
 {
 
 // Global Variables:
 details::splash_screen_context g_splash_screen;
 
-//------------------------------------------------------------------------------
+//-----------------------------------------------------------------------------------------------------------
 /**
 */
 void create_splash_screen(memory::base_allocator& allocator)
 {
-    g_splash_screen.worker_thread = sys::spawn_thread(allocator,
+    g_splash_screen.worker_thread = sys::spawn_thread(
         details::on_splash_screen, &g_splash_screen, L"splash screen",
         sys::thread_priority::medium, 32_kb, 0);
 
@@ -160,7 +136,7 @@ void create_splash_screen(memory::base_allocator& allocator)
         "Splash screen hasn't been created");
 }
 
-//------------------------------------------------------------------------------
+//-----------------------------------------------------------------------------------------------------------
 /**
 */
 void destroy_splash_screen()
@@ -177,4 +153,4 @@ void destroy_splash_screen()
 }
 
 } // namespace xr::paranoia
-//------------------------------------------------------------------------------
+//-----------------------------------------------------------------------------------------------------------
