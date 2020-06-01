ad117d0d7df7d112aff33e83ac84502c977abd06
Pavel <codepoet.vault@gmail.com>
2020-06-01 23:10:53 +0300

    game: constants refactoring

--- src/engine/game/sources/constants.h
+++ src/engine/game/sources/constants.h
@@ -1,21 +1,27 @@
 // This file is a part of xray-ng engine
 //
 
-namespace xr::constants
-{
+#include "corlib/macro/constexpr.h"
 
+//-----------------------------------------------------------------------------------------------------------
+XR_NAMESPACE_BEGIN(xr, contants)
+
+//-----------------------------------------------------------------------------------------------------------
 // shared
 
-constexpr const char xrayng_default_config[] = "config";
-constexpr const char xrayng_caption[] = "xray-ng";
+XR_CONSTEXPR_CPP14_OR_STATIC_CONST char xrayng_default_config[] = "config";
+XR_CONSTEXPR_CPP14_OR_STATIC_CONST char xrayng_caption[] = "xray-ng";
 
+//-----------------------------------------------------------------------------------------------------------
 // messages
 
-constexpr const char vulkan_support_error_msg[] = "Vulkan is not supported";
+XR_CONSTEXPR_CPP14_OR_STATIC_CONST char vulkan_support_error_msg[] = "Vulkan is not supported";
 
+//-----------------------------------------------------------------------------------------------------------
 // configuration file
 
-constexpr const char window_width[] = "r_window_width";
-constexpr const char window_height[] = "r_window_height";
+XR_CONSTEXPR_CPP14_OR_STATIC_CONST char window_width[] = "r_window_width";
+XR_CONSTEXPR_CPP14_OR_STATIC_CONST char window_height[] = "r_window_height";
 
-} // namespace xr::constants
+XR_NAMESPACE_END(xr, contants)
+//-----------------------------------------------------------------------------------------------------------
