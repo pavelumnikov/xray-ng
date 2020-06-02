47ecadf41c3e1397ffd5e78b6d228d2afb0739a8
Pavel <codepoet.vault@gmail.com>
2020-06-02 23:41:27 +0300

    sfx: uses refactored extension module

--- src/engine/modules/sfx/sources/fmod/fmod_sfx_api.h
+++ src/engine/modules/sfx/sources/fmod/fmod_sfx_api.h
@@ -15,15 +15,25 @@ class fmod_sfx_api : public api
 {
 public:
     fmod_sfx_api(memory::base_allocator& alloc);
-    virtual ~fmod_sfx_api();
+    virtual ~fmod_sfx_api() = default;
 
     virtual utils::shared_ptr<environment> create_environment() override;
     virtual void switch_to_environment(utils::shared_ptr<environment> const& env) override;
+
+    fmod_device& get_device();
 
 private:
     fmod_device m_device;
     fmod_event_descriptions m_event_descriptions;
 }; // class fmod_sfx_api
+
+//-----------------------------------------------------------------------------------------------------------
+/**
+ */
+inline fmod_device& fmod_sfx_api::get_device()
+{
+    return m_device;
+}
 
 XR_NAMESPACE_END(xr, sfx, fmod_impl)
 //-----------------------------------------------------------------------------------------------------------
