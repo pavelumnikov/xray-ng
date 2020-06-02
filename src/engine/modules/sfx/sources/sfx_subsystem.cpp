df3ff63cb09f5f710ffd3ff1e35bb7a8c767fc01
Pavel <codepoet.vault@gmail.com>
2020-06-02 23:36:47 +0300

    sfx: added initialization

--- src/engine/modules/sfx/sources/sfx_subsystem.cpp
+++ src/engine/modules/sfx/sources/sfx_subsystem.cpp
@@ -2,6 +2,7 @@
 //
 
 #include "sfx/sfx_subsystem.h"
+#include "fmod/fmod_sfx_api.h"
 #include "corlib/memory/allocator_macro.h"
 
 //-----------------------------------------------------------------------------------------------------------
@@ -11,8 +12,9 @@ XR_NAMESPACE_BEGIN(xr, sfx)
 /**
  */
 sfx_subsystem::sfx_subsystem(extension::context* ctx, memory::base_allocator& alloc)
-    : extension::subsystem(ctx)
-    , m_allocator(alloc)
+    : extension::subsystem { ctx }
+    , m_allocator { alloc }
+    , m_sfx_api { nullptr }
 {
     initialize_self();
 }
@@ -30,6 +32,10 @@ sfx_subsystem::~sfx_subsystem()
  */
 bool sfx_subsystem::initialize_async(tasks::execution_context& ctx)
 {
+    fmod_impl::fmod_sfx_api* api = static_cast<fmod_impl::fmod_sfx_api*>(m_sfx_api);
+    fmod_impl::fmod_device* device = &api->get_device();
+
+    device->initialize();
     return true;
 }
 
@@ -44,6 +50,8 @@ void sfx_subsystem::tick_async(tasks::execution_context& ctx, float delta)
  */
 void sfx_subsystem::initialize_self()
 {
+    m_sfx_api = XR_ALLOCATE_OBJECT_T(m_allocator, fmod_impl::fmod_sfx_api, "FMOD SFX API")(m_allocator);
+    XR_DEBUG_ASSERTION_MSG(m_sfx_api != nullptr, "failed to initialize FMOD SFX API");
 }
 
 //-----------------------------------------------------------------------------------------------------------
@@ -51,6 +59,7 @@ void sfx_subsystem::initialize_self()
  */
 void sfx_subsystem::destroy_self()
 {
+    XR_DEALLOCATE_MEMORY_T(m_allocator, m_sfx_api);
 }
 
 XR_NAMESPACE_END(xr, sfx)
